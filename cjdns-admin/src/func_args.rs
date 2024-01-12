f61fd8de9ed0eb226fa312353a70ce878cecbe5d
Ilyas Gasanov <torso.nafi@gmail.com>
2024-01-12 23:32:32 +0400

    Support JSON arguments in cjdns-admin (e.g. by cjdnstool cexec for types like List etc.)

--- cjdns-admin/src/func_args.rs
+++ cjdns-admin/src/func_args.rs
@@ -4,17 +4,48 @@ use std::collections::BTreeMap;
 
 use serde::ser::SerializeMap;
 use serde::{Serialize, Serializer};
+use serde_json::Value as JsonValue;
 
 /// Argument name (alias to `String`).
 pub type ArgName = String;
 
-/// Argument value (either integer or string).
+/// Argument value (either integer, string, or JSON).
 #[derive(Clone, PartialEq, Eq, Debug)]
 pub enum ArgValue {
     /// Integer argument value.
     Int(i64),
     /// String argument value.
     String(String),
+    /// JSON argument value.
+    Json(JsonValue),
+}
+
+impl From<i64> for ArgValue {
+    #[inline]
+    fn from(value: i64) -> Self {
+        ArgValue::Int(value)
+    }
+}
+
+impl From<String> for ArgValue {
+    #[inline]
+    fn from(value: String) -> Self {
+        ArgValue::String(value)
+    }
+}
+
+impl From<&str> for ArgValue {
+    #[inline]
+    fn from(value: &str) -> Self {
+        ArgValue::String(value.to_string())
+    }
+}
+
+impl From<JsonValue> for ArgValue {
+    #[inline]
+    fn from(value: JsonValue) -> Self {
+        ArgValue::Json(value)
+    }
 }
 
 /// Remote function argument values.
@@ -45,55 +76,60 @@ impl Serialize for ArgValues {
             match v {
                 ArgValue::Int(int_val) => encoder.serialize_entry(k, int_val)?,
                 ArgValue::String(str_val) => encoder.serialize_entry(k, str_val)?,
+                ArgValue::Json(json_val) => encoder.serialize_entry(k, json_val)?,
             }
         }
         encoder.end()
     }
 }
 
-#[test]
-fn test_args_ser() -> Result<(), Box<dyn std::error::Error>> {
-    let mut args = ArgValues::new();
-    args.add("foo".to_string(), ArgValue::String("bar".to_string()));
-    args.add("boo".to_string(), ArgValue::Int(42));
-    args.add("zoo".to_string(), ArgValue::Int(-42));
+#[cfg(test)]
+mod tests {
+    use super::{ArgValue, ArgValues};
 
-    let benc = String::from_utf8(bencode::to_bytes(&args)?)?;
-    assert_eq!(benc, "d3:booi42e3:foo3:bar3:zooi-42ee");
+    use serde_json::json;
 
-    Ok(())
-}
+    #[test]
+    fn test_args_ser() -> Result<(), Box<dyn std::error::Error>> {
+        let mut args = ArgValues::new();
+        args.add("foo".to_string(), ArgValue::String("bar".to_string()));
+        args.add("boo".to_string(), ArgValue::Int(42));
+        args.add("zoo".to_string(), ArgValue::Int(-42));
 
-impl From<i64> for ArgValue {
-    #[inline]
-    fn from(value: i64) -> Self {
-        ArgValue::Int(value)
-    }
-}
+        let benc = String::from_utf8(bencode::to_bytes(&args)?)?;
+        assert_eq!(benc, "d3:booi42e3:foo3:bar3:zooi-42ee");
 
-impl From<String> for ArgValue {
-    #[inline]
-    fn from(value: String) -> Self {
-        ArgValue::String(value)
-    }
-}
-
-impl From<&str> for ArgValue {
-    #[inline]
-    fn from(value: &str) -> Self {
-        ArgValue::String(value.to_string())
-    }
-}
-
-#[test]
-fn test_arg_value_conversion() {
-    fn arg<T: Into<ArgValue>>(v: T) -> ArgValue {
-        v.into()
+        Ok(())
     }
 
-    assert_eq!(arg(42), ArgValue::Int(42));
-    assert_eq!(arg(-42), ArgValue::Int(-42));
+    #[test]
+    fn test_args_ser_json() -> Result<(), Box<dyn std::error::Error>> {
+        let mut args = ArgValues::new();
+        args.add("foo".to_string(), ArgValue::Json(json!([42, -42, "bar"])));
+        args.add(
+            "boo".to_string(),
+            ArgValue::Json(json!({
+                "key1": "baz",
+                "key2": ["Lorem", "ipsum", {"dolor": ["sit", "amet"]}]
+            })),
+        );
 
-    assert_eq!(arg("foo"), ArgValue::String("foo".to_string()));
-    assert_eq!(arg("bar".to_string()), ArgValue::String("bar".to_string()));
+        let benc = String::from_utf8(bencode::to_bytes(&args)?)?;
+        assert_eq!(benc, "d3:bood4:key13:baz4:key2l5:Lorem5:ipsumd5:dolorl3:sit4:ameteeee3:fooli42ei-42e3:baree");
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_arg_value_conversion() {
+        fn arg<T: Into<ArgValue>>(v: T) -> ArgValue {
+            v.into()
+        }
+
+        assert_eq!(arg(42), ArgValue::Int(42));
+        assert_eq!(arg(-42), ArgValue::Int(-42));
+
+        assert_eq!(arg("foo"), ArgValue::String("foo".to_string()));
+        assert_eq!(arg("bar".to_string()), ArgValue::String("bar".to_string()));
+    }
 }
