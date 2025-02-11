14a6f093334ea5e6bae571ef8100c3306a1ca8ec
Caleb James DeLisle <cjd@cjdns.fr>
2025-02-11 20:57:40 +0000

    Change to dependency exports.
    Begin exporting sodiumoxide so cjdns can use it and have one version.
    Also add eth feature and not export anything with a dependency on ethereum code unless eth feature is passed.

--- cjdns/src/lib.rs
+++ cjdns/src/lib.rs
@@ -5,13 +5,23 @@ pub use cjdns_bytes as bytes;
 pub use cjdns_core as core;
 pub use cjdns_crypto as crypto;
 pub use cjdns_ctrl as ctrl;
-pub use cjdns_eth_rpc as eth_rpc;
 pub use cjdns_hdr as hdr;
 pub use cjdns_keys as keys;
-pub use cjdns_pkt_abi as pkt_abi;
-pub use cjdns_pns as pns;
+pub use netchecksum as checksum;
+pub use sodiumoxide;
 pub use cjdns_sniff as sniff;
 pub use cjdns_snode_wire as snode_wire;
 pub use cjdns_util as util;
+
+#[cfg(feature = "eth")]
+pub use cjdns_eth_rpc as eth_rpc;
+
+#[cfg(feature = "eth")]
+pub use cjdns_pkt_abi as pkt_abi;
+
+#[cfg(feature = "eth")]
+pub use cjdns_pns as pns;
+
+#[cfg(feature = "http")]
 pub use cjdns_util_http as http;
-pub use netchecksum as checksum;
+
